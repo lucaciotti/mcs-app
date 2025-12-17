fd3e9f652cdd9736fd7cb9afc5216bdea944ec70
Luca Ciotti <luca.ciotti@gmail.com>
2025-12-17 23:08:27 +0100

    Fix: export

--- app/Exports/InventorySimpleExport.php
+++ app/Exports/InventorySimpleExport.php
@@ -96,7 +96,7 @@ class InventorySimpleExport implements FromArray, WithMapping, WithHeadings, Sho
 
     public function map($row): array
     {
-        $body = [strval($row[0]), $row[1], $row[2] ?? '', $row[3], $row[4], $row[5], $row[6], $row[7], $row[8]];
+        $body = [strval($row[0]), $row[1], $row[2] ?? '', $row[3], $row[4], $row[5], $row[6], $row[7]];
         return $body;
     }
 }
